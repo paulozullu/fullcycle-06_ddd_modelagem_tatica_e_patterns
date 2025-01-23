09dab46b19be236cd8ca8e1d18466dfb1da8fc2a
Paulo Fabrício <paulozullu@gmail.com>
2025-01-23 14:14:48 -0300

    chore: adds domain folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
-import Order from "./entity/order";
-import OrderItem from "./entity/order_items";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
+import Order from "./domain/entity/order";
+import OrderItem from "./domain/entity/order_items";
 
 // aggregate (id)
 let customer = new Customer("123", "Paulo Fabrício");
